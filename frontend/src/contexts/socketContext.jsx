fbf2eaabd3d8c7601024bb8e78d74a87b518d938
Samir Khanal <gdssamir@gmail.com>
2024-10-10 12:40:02 +0545

    deploy testing

--- frontend/src/contexts/socketContext.jsx
+++ frontend/src/contexts/socketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8000", {
+      const socket = io("https://chatty-backend-1rc4.onrender.com", {
         query: {
           userId: authUser.data._id,
         },
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://chatty-backend-1rc4.onrender.com",
       },
     },
   },
