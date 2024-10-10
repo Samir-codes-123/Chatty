feaf414f24302f7544d931fb3336747ec20ddc94
Samir Khanal <gdssamir@gmail.com>
2024-10-10 12:38:06 +0545

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5174"],
+    origin: ["http://https://chatty-frontend-zhsu.onrender.com"],
     methods: ["GET", "POST"],
   },
 });
