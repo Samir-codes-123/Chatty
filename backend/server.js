4b0b425f742e5b097c255c7486a9ec042be8c24c
Samir Khanal <gdssamir@gmail.com>
2024-10-27 15:52:36 +0545

    removed home endpoint

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("hello world!!");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello world!!");
+// });
 
 //routes
 app.use("/api/auth", authRoutes);
